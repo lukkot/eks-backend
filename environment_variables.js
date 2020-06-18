2239889a3741d58d6bead90f63640dc1f3522bab
root <root@invescoci.gft.com>
2020-06-18 15:16:25 +0100

    Internal backend service added

--- environment_variables.js
+++ environment_variables.js
@@ -2,3 +2,7 @@ exports.DB_HOST = process.env.DB_HOST;
 exports.DB_PORT = process.env.DB_PORT;
 exports.DB_USER = process.env.DB_USER;
 exports.DB_PASSWORD = process.env.DB_PASSWORD;
+
+exports.BACKEND_INTERNAL_HOST = process.env.BACKEND_INTERNAL_HOST;
+exports.BACKEND_INTERNAL_PORT = process.env.BACKEND_INTERNAL_PORT;
+
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 'use strict';
 
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const superagent = require('superagent');
 
-const network = require('./ips.js')
+const network = require('./ips.js');
 const db = require('./db.js');
+const env = require('./environment_variables.js');
 
 
 // Constants
-const PORT = 8080;
+const PORT = 8081;
 const HOST = '0.0.0.0';
 const ips = network.getIps();
 
@@ -20,19 +22,25 @@ app.use(cors());
 // ENDPOINTS
 app.get('/', async (req, res) => {
   var dbResult = await db.getIpAndTimestamp();
-  
-  
+  var backendInternalResult = (await superagent.get(env.BACKEND_INTERNAL_HOST + ':' + env.BACKEND_INTERNAL_PORT)).text;
+
   var message = "";
-  message += "*** BACKEND:<br />"
-  message += ips + "<br />"
-  message += "<br />"
-  message += "*** DB:<br />"
-  message += dbResult[0].ip + "<br />"
-  message += dbResult[0].timestamp + "<br />"
-  
+
+  message += "*** BACKEND:<br />";
+  message += ips + "<br />";
+  message += "<br />";
+
+  message += "*** DB:<br />";
+  message += dbResult[0].ip + "<br />";
+  message += dbResult[0].timestamp + "<br />";
+  message += "<br />";
+  message += "<br />";
+
+  message += backendInternalResult;
+
   res.send(message);
 });
 
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Running on http:${HOST}:${PORT}`);
 
